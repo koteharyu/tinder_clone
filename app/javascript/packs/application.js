31a0ef726111922a681dbb9dd303b2126a5438f5
haryu <hryau6@gmail.com>
2021-12-14 15:21:27 +0900

    add: yarn add hammerjs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,8 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 import 'bootstrap'
-import '../stylesheets/application'
+import 'hammerjs';
+import '../stylesheets/application';
 import '@fortawesome/fontawesome-free/js/all';
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
