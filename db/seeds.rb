da53056d50367d875f4dfda7f91b3546e1ef28b4
haryu <hryau6@gmail.com>
2021-12-14 14:55:47 +0900

    add: seed of User

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,48 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+  email: 'user1@gmail.com',
+  password: 'password',
+  name: 'エマ',
+  self_introduction: '音楽と猫と美味しいものが好き！',
+  gender: 1,
+  avatar: open("#{Rails.root}/db/dummy_images/1.jpeg")
+)
+User.create!(
+  email: 'user2@gmail.com',
+  password: 'password',
+  name: 'オリビア',
+  self_introduction: 'クリエイターさんと話してみたい。',
+  gender: 1,
+  avatar: open("#{Rails.root}/db/dummy_images/2.jpeg")
+)
+User.create!(
+  email: 'user3@gmail.com',
+  password: 'password',
+  name: 'エヴァ',
+  self_introduction: 'プロフィールをご覧いただきありがとうございます。東京でWebマーケティング関連の仕事をしています。',
+  gender: 1,
+  avatar: open("#{Rails.root}/db/dummy_images/3.jpeg")
+)
+User.create!(
+  email: 'user4@gmail.com',
+  password: 'password',
+  name: 'ノア',
+  self_introduction: '東京で美容師をしています。',
+  gender: 0,
+  avatar: open("#{Rails.root}/db/dummy_images/4.jpeg")
+)
+User.create!(
+  email: 'user5@gmail.com',
+  password: 'password',
+  name: 'リアム',
+  self_introduction: '普段は公認会計士として働いています',
+  gender: 0,
+  avatar: open("#{Rails.root}/db/dummy_images/5.jpeg")
+)
+User.create!(
+  email: 'user6@gmail.com',
+  password: 'password',
+  name: 'オリバー',
+  self_introduction: '週3日くらい1人ラーメンします',
+  gender: 0,
+  avatar: open("#{Rails.root}/db/dummy_images/6.jpeg")
+)
