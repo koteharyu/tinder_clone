9e66becf8563677262810001f9586a1fbbdff420
haryu <hryau6@gmail.com>
2021-12-14 14:39:21 +0900

    add: preview an avatar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "../src/avatar_upload"
 
 Rails.start()
 Turbolinks.start()
