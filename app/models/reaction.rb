f6aa765158502ead0a067498c7f9b776683abdf8
haryu <hryau6@gmail.com>
2021-12-15 15:16:44 +0900

    fix: typo

--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -21,6 +21,6 @@
 #
 class Reaction < ApplicationRecord
 
-  belongs_to :to_user, class_name 'User'
-  belongs_to :from_user, class_name 'User'
+  belongs_to :to_user, class_name: "User"
+  belongs_to :from_user, class_name: 'User'
 end
